39fd93123738f46290c019584df1ccf603bb8462
VadimKorobec <_s_e_t__@ukr.net>
2023-01-29 14:39:25 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,33 +2,26 @@ import { ContactsForm } from 'components/ContactsForm/ContactsForm';
 import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Filter } from 'components/Filter/Filter';
 import { Container } from './App.styled';
+import { nanoid } from 'nanoid';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const contactsStorage = localStorage.getItem('contacts');
-    const persedContacts = JSON.parse(contactsStorage);
-
-    if (persedContacts) {
-      setContacts(persedContacts);
-    } else {
-      return;
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  // добавляет контакт в список
   const addContact = ({ name, number }) => {
     const findName = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
@@ -57,8 +50,8 @@ export const App = () => {
     );
   };
 
-  const handleFilter = e => {
-    setFilter(e.currentTarget.value);
+  const handleFilter = event => {
+    setFilter(event.currentTarget.value);
   };
 
   const filterContacts = () => {
