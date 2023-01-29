ad48c141d03d8ad60920511923801a2194e9a90a
VadimKorobec <_s_e_t__@ukr.net>
2023-01-29 14:00:37 +0200

    Create App.styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  padding: 15px 30px;
+`;
+
+export const Section = styled.section`
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
